787e71c2b3fc2b92e00c6b95d17db03ff4973e04
gon125 <geonhyeong.lim@icloud.com>
2020-04-26 19:17:22 +0900

    Update App.js

--- KNUGra/App.js
+++ KNUGra/App.js
@@ -1,12 +1,65 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Platform, StatusBar } from 'react-native';
+import { SplashScreen } from 'expo';
+import { Ionicons } from '@expo/vector-icons';
+//import { NavigationContainer } from '@react-navigation/native';
+//import { createStackNavigator } from '@react-navigation/stack';
+import LoginView from './UI/views/LoginView';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-export default function App() {
-  return (
+
+//import BottomTabNavigator from './UI/navigation/BottomTabNavigator';
+//import useLinking from './UI/navigation/useLinking';
+
+//const Stack = createStackNavigator();
+
+export default function App(props) {
+
+  const [isLoadingComplete, setLoadingComplete] = React.useState(false);
+  const [isUserLoggedIn, setUserLoggedIn] = React.useState(false);
+  const [initialNavigationState, setInitialNavigationState] = React.useState();
+  const containerRef = React.useRef();
+  //const { getInitialState } = useLinking(containerRef);
+
+  // Load any resources or data that we need prior to rendering the app
+  React.useEffect(() => {
+    async function loadResourcesAndDataAsync() {
+      try {
+        SplashScreen.preventAutoHide();
+
+        // Load our initial navigation state
+        setInitialNavigationState(await getInitialState());
+
+        // Load fonts if we want
+      } catch(e) {
+        console.warn(e);
+      } finally {
+        setLoadingComplete(true);
+        SplashScreen.hide();
+      }
+    }
+
+    loadResourcesAndDataAsync();
+  }, []);
+
+  if (!isLoadingComplete && !props.skipLoadingScreen) {
+    return null;
+  } else if (!isUserLoggedIn) {
+    return (
+    <SafeAreaProvider>
+      <LoginView />
+    </SafeAreaProvider>
+    );
+  } else return null; /*(
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {Platform.OS === 'ios' && <StatusBar barStyle='default' />}
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Root" component={BottomTabNavigator} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </View>
-  );
+  ); */
 }
 
 const styles = StyleSheet.create({
