c1ab5e3bc18edc446277b445dbbb756a12d4683c
gon125 <geonhyeong.lim@icloud.com>
2020-05-09 16:31:22 +0900

    Update GInfoCheckView.js
    added basic UI

--- KNUGra/UI/views/GInfoCheckView.js
+++ KNUGra/UI/views/GInfoCheckView.js
@@ -1,25 +1,164 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, SectionList } from 'react-native';
+import MajorPicker from '../components/MajorPicker';
+import { Font } from '../constants/Constants';
 
 export default function GInfoCheckView() {
+    const [selectedPickerValue, setSelectedPickerValue] = React.useState('심화컴퓨터전공(ABEEK)');
+
+
+
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>gigigi</Text>
+            <View style={styles.pickerArea}>
+                <MajorPicker
+                    selectedPickerValue={selectedPickerValue}
+                    setSelectedPickerValue={setSelectedPickerValue}
+                />
+            </View>
+            <View style={styles.listArea}>
+                <SectionList
+                    sections={DATA}
+                    keyExtractor={(item, index) => item + index}
+                    renderItem={({ item }) => <Item name={item.name} value={item.value}></Item>}
+                    renderSectionHeader={({section: {title}}) => <Header style={styles.header} title={title}/>}
+                >
+
+                </SectionList>
+            </View>
         </View>
     );
 }
+
+function Header({title}) {
+
+    switch(title) {
+        case '졸업요건': 
+            return( 
+                <View style={styles.header}>
+                    <Text style={styles.headerText}>{title}</Text>
+                    <View style={styles.subHeader}>
+                        <Text style={styles.subHeaderText}>항목</Text>
+                        <View style={styles.spacer}></View>
+                        <Text style={styles.subHeaderText}>졸업기준</Text>
+                    </View>
+                </View>
+            );
+        case '필수 교과목':
+            return( 
+                <View style={styles.header}>
+                    <Text style={styles.headerText}>{title}</Text>
+                    <View style={styles.subHeader}>
+                        <Text style={styles.subHeaderText}>교과목명</Text>
+                    </View>
+                </View>
+            );
+        default:
+            return (
+                <View style={styles.header}>
+                    <Text style={styles.headerText}>{title}</Text>
+                    <View style={styles.subHeader}>
+                        <Text style={styles.subHeaderText}>교과목명</Text>
+                    </View>
+                </View>
+            );
+    }
+}
+
+function Item({name, value}) {
+
+    if (value === null) {
+        return (
+            <View style={styles.item}>
+                <Text style={styles.itemText}>{name}</Text>
+            </View>
+        );
+    } else {
+        return (
+            <View style={styles.item}>
+                <Text style={styles.itemText}>{name}</Text>
+                <View style={styles.spacer}></View>
+                <Text style={styles.itemText}>{value}</Text>
+            </View>
+        );
+    }
+}
+
+const DATA = [
+    {
+      title: '졸업요건',
+      data: [{name: '이수학점', value: '150학점'}, {name: '기본소양', value: '15학점'}, {name: '전공기반', value: '22학점'}],
+    },
+    {
+      title: '필수 교과목',
+      data: [{name: '자료구조'}, {name: '시스템프로그래밍'}, {name: '종합설계프로젝트2'}],
+    },
+    {
+      title: '선택 교과목',
+      data: [{name: '이수학점', value: '150학점'}, {name: '이수학점', value: '150학점'}, {name: '이수학점', value: '150학점'}],
+    },
+    {
+      title: '하하',
+      data: [{name: '이수학점', value: '150학점'}, {name: '이수학점', value: '150학점'}, {name: '이수학점', value: '150학점'}],
+    },
+];
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
     },
 
+    spacer: {
+        width: '50%',
+    },
+
+    pickerArea: {
+        flex: 1,
+        alignItems:'baseline',
+        paddingLeft: '3%',
+        paddingTop: '3%',
+    },
+
+    listArea: {
+        flex: 9,
+        paddingHorizontal: '10%',
+    },
+    
     text: {
-        fontSize: 26, 
         color: '#000',
-    }
+    },
+
+    item: {
+        flexDirection: 'row',
+        padding: 20,
+
+        borderBottomColor: 'black',
+        borderBottomWidth: 1,
+        textAlignVertical: 'bottom',
+    },  
+    header: {
+        marginTop: 3,
+        backgroundColor: '#fff',
+        borderBottomColor: 'black',
+        borderBottomWidth: 3,
+    },
+    subHeader: {
+        flexDirection: 'row',
+        marginVertical: 8,
+        textAlign: 'center',
+    },  
+    headerText: {
+        alignSelf: 'center',
+        fontWeight: 'bold',
+        marginTop: 10,
+        fontSize: Font.size - 8,
+    },
+    subHeaderText: {
+        alignSelf: 'center',
+        fontWeight: 'bold',
+        marginTop: 10,
+    },
+
 });
 
