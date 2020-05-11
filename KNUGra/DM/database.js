4bd2bc6d0544e012cccfd4ef02a82771d59a9455
Hyunsu <hyunsujang1227@gmail.com>
2020-05-11 17:26:41 +0900

    modify DM/Database.js

--- KNUGra/DM/database.js
+++ KNUGra/DM/database.js
@@ -1,34 +1,55 @@
 let instance = null;
 
-class database {
+class Database {
     constructor(){
         if(instance) return instance;
 
-        this.requiredSubjectList = new subjectList();
-        this.designSubjectList = new subjectList();
-        this.startupSubjectList = new subjectList();
-        this.recommendedSubjectList = new subjectList();
+        this.requiredSubjectList = new SubjectList();
+        this.designSubjectList = new SubjectList();
+        this.startupSubjectList = new SubjectList();
+        this.recommendedSubjectList = new SubjectList();
+        this.graduationInfoLists = new GraduationInfoList();
 
         instance = this;
     }
 
     load(){
-        designSubjectList = this.loadDesignSubjectList();
-        requiredSubjectLists = this.loadRequiredSubjectLists();
-        startupSubjectList = this.loadStartupSubjectList();
-        recommendedSubjectLists = this.loadRecommendedSubjectLists();
+        this.designSubjectList = this.loadDesignSubjectList();
+        this.requiredSubjectLists = this.loadRequiredSubjectLists();
+        this.startupSubjectList = this.loadStartupSubjectList();
+        this.recommendedSubjectLists = this.loadRecommendedSubjectLists();
+        this.graduationInfoLists = this.loadGraduationInfoLists();
     }
 
     loadDesignSubjectList(){
-        
+        this.designSubjectList = designSubjectList;
     }
     loadRequiredSubjectLists(){
-
+        this.requiredSubjectList = requiredSubjectList;
     }
     loadStartupSubjectList(){
-
+        this.startupSubjectList = startupSubjectList;
     }
     loadRecommendedSubjectLists(){
+        this.recommendedSubjectList = recommendedSubjectList;
+    }
+    loadGraduationInfoLists(){
+        this.graduationInfoLists = graduationInfoLists;
+    }
 
+    getDesignSubjectList(){
+        return this.designSubjectList;
+    }
+    getRequiredSubjectLists(){
+        return this.requiredSubjectList;
+    }
+    getStartupSubjectList(){
+        return this.startupSubjectList;
+    }
+    getRecommendedSubjectLists(){
+        return this.recommendedSubjectList;
+    }
+    getGraduationInfoLists(){
+        return this.graduationInfoLists;
     }
 }
