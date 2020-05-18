570307efdfa9c5e7936f6140a775767c9bae243f
Hyunsu <hyunsujang1227@gmail.com>
2020-05-18 23:57:40 +0900

    add DM\Student.js

---
+++ KNUGra/DM/Student.js
@@ -0,0 +1,14 @@
+function Student() {
+    this.careerList = null;
+    this.completedSubjectList = null;
+    
+    this.getStudent = function() {
+        return this;
+    }
+    this.setCareerList = function(careerList) {
+        this.careerList = careerList;
+    }
+    this.setCompletedSubjectList = function(completedSubjectList){
+        this.completedSubjectList = completedSubjectList;
+    }
+}
