3a4c8cae86ba646113faec3cc1bfa996851d14f6
Hyunsu <hyunsujang1227@gmail.com>
2020-05-18 23:56:28 +0900

    add DM\ServerConnect.js

---
+++ KNUGra/DM/ServerConnect.js
@@ -0,0 +1,18 @@
+const getDataFromServer = function(id , pw, major, requesttype){
+
+}
+
+function ServerConnect() {
+    this.hostIP = null;
+
+    this.login = function(id, pw, major) {
+        
+        return true;
+    }
+    this.logout = function(id) {
+        
+    }
+    this.updateStudent = function(id, major){
+
+    }
+}
