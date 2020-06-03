791c43851c99f31a883fc3b1589c5ff8615abbce
gon125 <geonhyeong.lim@icloud.com>
2020-06-03 16:11:23 +0900

    Update ServerConnect.js
    fixed compatability problem

--- KNUGra/DM/ServerConnect.js
+++ KNUGra/DM/ServerConnect.js
@@ -1,12 +1,10 @@
-import Database from "./Database";
-
 export default class ServerConnect {
     #hostIP;
 
     constructor(){
         this.#hostIP = null;
     }
-    #getDataFromServer(id , pw, major, requesttype){
+    getDataFromServer(id , pw, major, requesttype){
 
     }
     login(id, pw, major) {
