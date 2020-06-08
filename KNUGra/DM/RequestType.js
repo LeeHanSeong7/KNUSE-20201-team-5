958b5206ec4565938353bfd8e0b5b704bdda223b
Hyunsu <hyunsujang1227@gmail.com>
2020-06-08 17:37:46 +0900

    add RequestType.js

---
+++ KNUGra/DM/RequestType.js
@@ -0,0 +1,20 @@
+const RequestType = {
+    LOGIN : "login",
+    UPDATE : "getGradeInfo",
+    LOGOUT : "logout",
+
+
+    COMPUTPER_ABEEK : "abeek",
+    GLOBAL_SOFTWARE_DOUBLE_MAJOR : "global",
+    GLOBAL_SOFTWARE_OVERSEAS_UNIV : "global",
+    GLOBAL_SOFTWARE_MASTERS_CHAINING : "global",
+
+    FINTECH : "global",
+    BIGDATA : "global",
+    MEDIAART : "global",
+    CONSTRUCTION_IT : "global",
+
+    DEFAULT : "global"
+}
+
+export default RequestType;
