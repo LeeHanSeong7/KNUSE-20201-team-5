e1ed7f4b87300476d88b4aa3e79c5f597219e87f
Hyunsu <vhtmxmqhdl@gmail.com>
2020-06-09 23:21:48 +0900

    Update ServerConnect.js

--- KNUGra/DM/ServerConnect.js
+++ KNUGra/DM/ServerConnect.js
@@ -1,3 +1,8 @@
+const NO_PROBLEM = 0
+const ID_PASSWARD_INCORRECT = 1
+const PASSWORD_CHANGE_DATE_THREE_MONTHS = 2
+const EXCEPTION = 3
+
 export default class ServerConnect {
     #hostIP;
 
@@ -5,10 +10,17 @@ export default class ServerConnect {
         this.#hostIP = null;
     }
     getDataFromServer(id , pw, major, requesttype){
-
+        
     }
     login(id, pw, major) {
-        return true;
+        let result = this.getDataFromServer(id,pw,major,"login");
+
+        if(result["login"] == "success"){
+            return true;
+        }
+        else{
+            return result["errorCode"];
+        }
     }
     logout(id) {
         
